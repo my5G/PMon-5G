4da3248863ea2d59154e6697c16777b10392d686
karllacr <karllabiancachaves@gmail.com>
2021-09-10 15:13:48 -0300

    Changing some files

--- XenVM/VMDownloader.sh
+++ XenVM/VMDownloader.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-fileid="1S7OwQZEvCwPEQxL7dm7Zz14YN1bOo2mX"
-filename="expran.zip"
+fileid="12SCRqBGAOJwe3BxknTTarrPd7IJX9uR0"
+filename="vm-image.zip"
+
 curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}" > /dev/null
+
 curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" -o ${filename}
 
 unzip ${filename}
