3ae24573d2afce503d10d5c0f31a8a59e52b5718
Karlla Chaves <karllabiancachaves@gmail.com>
2021-09-17 11:27:41 -0300

    Update VMDownloader.sh

--- XenVM/VMDownloader.sh
+++ XenVM/VMDownloader.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-fileid="1SF6A7tdrjqnpJqOSWyjfQZzgrIAorgiE"
-filename="vm-image.zip"
+fileid="1HfzV2kBwXdGKGcJGnOtwaADhXtcn0yt5"
+filename="copy-vm-image.zip"
 
 curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}" > /dev/null
 
 curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" -o ${filename}
 
-jar xvf ${filename}
+unzip ${filename}
 
 mv expran-disk pmon-disk
 mv expran-swap pmon-swap
